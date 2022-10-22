aca3eca3a6b2581a02ededa1220adb712893c4f5
Edward Fu <edwardfu850808@gmail.com>
2022-10-22 10:33:17 +0000

    add url id query

--- backend/url_shortener/models/url/graphql/queries.py
+++ backend/url_shortener/models/url/graphql/queries.py
@@ -6,6 +6,10 @@ from .types import UrlType
 
 class UrlQueries(graphene.ObjectType):
     urls = graphene.List(UrlType)
+    url = graphene.Field(UrlType, id=graphene.ID(description="Id of the URL", required=True))
 
     def resolve_urls(root, info):
         return Url.objects.all()
+
+    def resolve_url(root, info, id):
+        return Url.objects.get(id=id)
